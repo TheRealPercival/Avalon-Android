abfbc3be43298263e66b277ee05bf885c13bb3d6
Drew Pusey <drewpusey654@gmail.com>
2026-07-29 20:10:07 -0500

    Switch font to ttf

--- app/src/main/java/com/therealpercival/avalon/MainActivity.kt
+++ app/src/main/java/com/therealpercival/avalon/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.therealpercival.avalon.presentation.setup.SetupScreen
 import com.therealpercival.avalon.presentation.ui.theme.AvalonTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/therealpercival/avalon/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/therealpercival/avalon/presentation/ui/theme/Type.kt
@@ -2,26 +2,17 @@ package com.therealpercival.avalon.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.googlefonts.Font
-import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
 import com.therealpercival.avalon.R
 
-val provider = GoogleFont.Provider(
-    providerAuthority = "com.google.android.gms.fonts",
-    providerPackage = "com.google.android.gms",
-    certificates = R.array.com_google_android_gms_fonts_certs
-)
-
-val fontName = GoogleFont("Livvic")
-
 val LivvicFontFamily = FontFamily(
-    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.Normal),
-    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.Medium),
-    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.SemiBold),
-    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.Bold)
+    Font(resId = R.font.livvic_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.livvic_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.livvic_semibold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.livvic_bold, weight = FontWeight.Bold)
 )
 
 // Set of Material typography styles to start with
